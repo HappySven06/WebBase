1ec66204df870d1353c20aecd96a13a3371918f5
Sven Laht <104273841+HappySven06@users.noreply.github.com>
2023-10-12 16:26:41 +0300

    Add files via upload

---
+++ V2/js/Footer.js
@@ -0,0 +1,36 @@
+function createFooter()
+{
+    var div = document.getElementById("footer");
+    var footer = '<div class="information"><div id="div1"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div><div id="div2"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div><div id="div3"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div></div><p class="rights">Kõik õigused kaitstud ©2023 - </p>';
+    div.innerHTML += footer;
+}
+
+/*
+<div class="information">
+    <div id="div1">
+        <h3>Võta meiega ühendus</h3>
+        <ul>
+            <li>Telefon: </li>
+            <li>E-mail: </li>
+        </ul>
+    </div>
+    <div id="div2">
+        <h3>Võta meiega ühendus</h3>
+        <ul>
+            <li>Telefon: </li>
+            <li>E-mail: </li>
+        </ul>
+    </div>
+    <div id="div3">
+        <h3>Võta meiega ühendus</h3>
+        <ul>
+            <li>Telefon: </li>
+            <li>E-mail: </li>
+        </ul>
+    </div>
+</div>
+
+<p class="rights">Kõik õigused kaitstud ©2023 - </p>
+
+<div class="information"><div id="div1"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div><div id="div2"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div><div id="div3"><h3>Võta meiega ühendus</h3><ul><li>Telefon: </li><li>E-mail: </li></ul></div></div>
+*/
---
+++ V2/js/MenuPar.js
@@ -0,0 +1,10 @@
+function createMenu()
+{
+    var div = document.getElementById("header");
+    var header = '<div class="navbar"><a onclick="mainMenue();">Menu</a></div>';
+    div.innerHTML += header;
+}
+
+/*<div class="navbar">
+    <a onclick="mainMenue();">Menu</a>
+</div>*/
